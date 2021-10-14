0434577f33d6366cac8bae6cd40f1f3791c0a98e
AmudhaAAA <MY_NAME@example.com>
2021-10-14 12:41:33 +1100

    kjhg

---
+++ 02goodbyeworld.py
@@ -0,0 +1,12 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    02GoodbyeWorld                              |")
+print("|    Name : Amudha Bharathi                      |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+print("Hello World")
+input("------> Program paused - press enter to continue")
+print("Goodbye World")
+
+
