5f104bbc96be920e71cdde6e2ed464de46881e78
AmudhaAAA <MY_NAME@example.com>
2021-10-27 21:20:25 +1100

    ;lkj

---
+++ 06whileLoop.py
@@ -0,0 +1,23 @@
+
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Amudha Bharathi                      |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+while True:
+    subject = input("What is the name of this subject ")
+    if subject == "IST":
+        print("")
+        print("")
+        print(" Congratulations!!")
+        print("")
+        print("")
+        break
+    else:
+        print("Not Correct - try again")
+    
+
+
+    
